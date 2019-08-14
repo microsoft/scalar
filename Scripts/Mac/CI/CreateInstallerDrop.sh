8038745f484a62af23377ed79855a7928dee1988
John Briggs <jobriggs@microsoft.com>
2019-08-14 12:53:12 -0400

    Publish installers on macOS

---
+++ Scripts/Mac/CI/CreateInstallerDrop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. "$(dirname ${BASH_SOURCE[0]})/../InitializeEnvironment.sh"
+
+CONFIGURATION=$1
+BUILDDROP_ROOT=$2
+if [ -z $BUILDDROP_ROOT ] || [ -z $CONFIGURATION ]; then
+  echo 'ERROR: Usage: CreateInstallerDrop.sh [configuration] [build drop root directory]'
+  exit 1
+fi
+
+# Set up the installer directory structure
+rm -rf $BUILDDROP_ROOT
+mkdir -p $BUILDDROP_ROOT
+
+# Copy to the build drop, retaining directory structure.
+
+# Copy to the build drop, retaining directory structure.
+cp $Scalar_OUTPUTDIR/Git/*.dmg $BUILDDROP_ROOT
+cp $Scalar_OUTPUTDIR/Scalar.Installer.Mac/bin/x64/$CONFIGURATION/netcoreapp2.1/osx-x64/*.pkg $BUILDDROP_ROOT
