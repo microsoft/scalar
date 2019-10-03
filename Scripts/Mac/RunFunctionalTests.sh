7f0803104a6bed4101091bf1d3a4a0a79babac52
Derrick Stolee <dstolee@microsoft.com>
2019-10-03 10:50:07 -0400

    Pipelines: Create build to run functional tests on a private repo
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Scripts/Mac/RunFunctionalTests.sh
+++ Scripts/Mac/RunFunctionalTests.sh
@@ -7,6 +7,9 @@ if [ -z $CONFIGURATION ]; then
   CONFIGURATION=Debug
 fi
 
+# consume $1 so the next "$@" is all arguments after it
+shift
+
 mkdir ~/Scalar.FT
 
-$Scalar_PUBLISHDIR/Scalar.FunctionalTests --full-suite $2
+$Scalar_PUBLISHDIR/Scalar.FunctionalTests --full-suite "$@"
