695821fbaa746c18bb4e31b8f43fd61016f8aefa
William Baker <wilbaker@microsoft.com>
2019-10-23 12:17:58 -0700

    Improve logging

--- Scalar.Service/MaintenanceTaskScheduler.cs
+++ Scalar.Service/MaintenanceTaskScheduler.cs
@@ -159,14 +159,27 @@ namespace Scalar.Service
 
             public void Execute()
             {
-                this.tracer.RelatedInfo($"{nameof(MaintenanceTask)}: Executing '{this.task}'");
                 UserAndSession registeredUser = this.getRegisteredUser();
                 if (registeredUser != null)
                 {
+                    EventMetadata metadata = new EventMetadata();
+                    metadata.Add(nameof(registeredUser.UserId), registeredUser.UserId);
+                    metadata.Add(nameof(registeredUser.SessionId), registeredUser.SessionId);
+                    metadata.Add(nameof(this.task), this.task);
+                    metadata.Add(TracingConstants.MessageKey.InfoMessagem "Executing maintenance task");
+                    this.tracer.RelatedEvent(
+                        EventLevel.Informational,
+                        $"{nameof(MaintenanceTaskScheduler)}.{nameof(this.Execute)}",
+                        metadata);
+
                     this.repoRegistry.RunMaintenanceTaskForRepos(
                         this.task,
                         registeredUser.UserId,
                         registeredUser.SessionId);
+                }
+                else
+                {
+                    this.tracer.RelatedInfo($"{nameof(MaintenanceTask)}: Skipping '{this.task}', no registered user");
                 }
             }
 
