0b09d5ffff0999d7af9ca96cf750e6bec55ec8a3
Derrick Stolee <dstolee@microsoft.com>
2019-12-13 11:34:09 -0500

    MaintenanceVerbTests: add basic tests
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

---
+++ Scalar.FunctionalTests/Tests/GitEnlistmentPerFixture/MaintenanceVerbTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Scalar.Tests.Should;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scalar.FunctionalTests.Tests.GitEnlistmentPerFixture
+{
+    [Category(Categories.GitRepository)]
+    public class MaintenanceVerbTests : TestsWithGitEnlistmentPerFixture
+    {
+        [TestCase]
+        [Order(1)]
+        public void CommitGraphStep()
+        {
+            this.Enlistment.CommitGraphStep();
+        }
+
+        [TestCase]
+        [Order(2)]
+        public void PackfileMaintenanceStep()
+        {
+            this.Enlistment
+                .PackfileMaintenanceStep()
+                .ShouldNotContain(false, "Skipping pack maintenance due to no .keep file.");
+        }
+
+        [TestCase]
+        [Order(3)]
+        public void LooseObjectsStep()
+        {
+            this.Enlistment.LooseObjectStep();
+        }
+    }
+}
