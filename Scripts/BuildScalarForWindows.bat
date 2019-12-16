89ce3613fbd1f957d141418ec70d0db626e69210
Matthew John Cheetham <mjcheetham@outlook.com>
2019-12-16 13:42:34 +0000

    Show normal output in Windows MSBuild

--- Scripts/BuildScalarForWindows.bat
+++ Scripts/BuildScalarForWindows.bat
@@ -9,6 +9,6 @@ IF "%2"=="" (SET "ScalarVersion=0.2.173.2") ELSE (SET "ScalarVersion=%2")
 REM Restore using the NuGet CLI rather than the dotnet CLI to workaround a bug
 REM in the version of NuGet included with the dotnet CLI (https://github.com/NuGet/Home/issues/8692)
 nuget restore %SCALAR_SRCDIR%\Scalar.sln || exit /b 1
-dotnet publish --no-restore %SCALAR_SRCDIR%\Scalar.sln -p:ScalarVersion=%ScalarVersion% --configuration %Configuration% --runtime win-x64 || exit /b 1
+dotnet publish --no-restore %SCALAR_SRCDIR%\Scalar.sln -p:ScalarVersion=%ScalarVersion% --configuration %Configuration% --runtime win-x64 -v:n || exit /b 1
 
 ENDLOCAL
