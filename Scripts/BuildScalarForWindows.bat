61cf12b5342d54232c4b3df411d3c53c6da59165
Matthew John Cheetham <mjcheetham@outlook.com>
2020-01-30 14:42:03 +0000

    Target Windows 10 64-bit specifically
    Because we have a dependency on PowerShell, which itself has native
    dependencies that are tied to specific Windows versions, we must pick a
    (minimum) Windows version for Scalar to support.
    
    We select Windows 10, mainly because Windows 7 is now out of support,
    and we only test for Windows 10.

--- Scripts/BuildScalarForWindows.bat
+++ Scripts/BuildScalarForWindows.bat
@@ -6,6 +6,6 @@ CALL %~dp0\InitializeEnvironment.bat || EXIT /b 10
 IF "%1"=="" (SET "Configuration=Debug") ELSE (SET "Configuration=%1")
 IF "%2"=="" (SET "ScalarVersion=0.2.173.2") ELSE (SET "ScalarVersion=%2")
 
-dotnet publish %SCALAR_SRCDIR%\Scalar.sln -p:ScalarVersion=%ScalarVersion% --configuration %Configuration% --runtime win-x64 -v:n || exit /b 1
+dotnet publish %SCALAR_SRCDIR%\Scalar.sln -p:ScalarVersion=%ScalarVersion% --configuration %Configuration% --runtime win10-x64 -v:n || exit /b 1
 
 ENDLOCAL
