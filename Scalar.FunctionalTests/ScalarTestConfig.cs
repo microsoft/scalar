a4cee25d6d9a3cee92ead14e52e0db04d3491967
Chris Darroch <chrisd@apache.org>
2020-09-11 00:28:28 -0700

    FunctionalTests: support variable local cache root
    We update the CloneWithDefaultLocalCacheLocation() functional
    test so it accommodates variable local cache paths on Linux,
    depending on whether $XDG_CACHE_HOME is set or not.  This allows
    the test to succeed on developer systems where we want to avoid
    polluting the default ~/.cache/scalar directory during test runs
    and so set $XDG_CACHE_HOME explicitly.
    
    Note that the new ScalarTestConfig.DefaultLocalCacheRoot accessor
    returns a path on Windows which should be "C:\.scalarCache",
    to match what would be supplied by the TryGetDefaultLocalCacheRoot()
    method of the WindowsPlatform if it were passed the default
    functional test enlistment root path.  However, this Windows-
    specific DefaultLocalCacheRoot value is unused, as the only
    functional test which uses it is CloneWithDefaultLocalCacheLocation()
    and that test belongs to the POSIXOnly category.

--- Scalar.FunctionalTests/ScalarTestConfig.cs
+++ Scalar.FunctionalTests/ScalarTestConfig.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Scalar.FunctionalTests
 {
@@ -9,6 +11,40 @@ namespace Scalar.FunctionalTests
         public static bool NoSharedCache { get; set; }
 
         public static string LocalCacheRoot { get; set; }
+
+        public static string DefaultLocalCacheRoot {
+            get
+            {
+                string homeDirectory = null;
+                string cachePath = ".scalarCache";
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    homeDirectory = Path.GetPathRoot(Properties.Settings.Default.EnlistmentRoot);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    homeDirectory = Environment.GetEnvironmentVariable("HOME");
+                }
+                else
+                {
+                    // On Linux we use a local cache path per the XDG Base Directory Specification.
+                    homeDirectory = Environment.GetEnvironmentVariable("XDG_CACHE_HOME");
+                    if (!string.IsNullOrEmpty(homeDirectory))
+                    {
+                        cachePath = "scalar";
+                    }
+                    else
+                    {
+                        homeDirectory = Environment.GetEnvironmentVariable("HOME");
+                        cachePath = Path.Combine(".cache", "scalar");
+                    }
+
+                }
+
+                return Path.Combine(homeDirectory, cachePath);
+            }
+        }
 
         public static object[] FileSystemRunners { get; set; }
 
