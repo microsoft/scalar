08fa0b6457d0ccfa6501cd0badabd10f3be30859
Chris Darroch <chrisd8088@github.com>
2020-10-09 00:45:16 -0700

    ScalarEnlistment: undo partial classes
    The GVFS/GVFS.Common/GVFSEnlistment.cs file was split into two
    and GVFS/GVFS.Common/GVFSEnlistment.Shared.cs was created in
    commit ed0479021d547cb8d79905e5b510245879782f3c, in order to
    share the IsUnattended() method with the GVFS.Hooks project.
    
    Given that there is no longer a corresponding Scalar version of
    the GVFS.Hooks project, there is no need to split out certain
    (static) methods from the others in any classes.  (The GVFS.Hooks
    project was removed in commit
    f12effc371d7b1457ab58c6d44959632404e5998 in PR #23.)
    
    Thus we merge the IsUnattended() method into ScalarEnlistment.cs
    and remove the ScalarEnlistment.Shared.cs file.

--- Scalar.Common/ScalarEnlistment.cs
+++ Scalar.Common/ScalarEnlistment.cs
@@ -1,11 +1,13 @@
 using Scalar.Common.FileSystem;
 using Scalar.Common.Git;
+using Scalar.Common.Tracing;
 using System;
 using System.IO;
+using System.Security;
 
 namespace Scalar.Common
 {
-    public partial class ScalarEnlistment : Enlistment
+    public class ScalarEnlistment : Enlistment
     {
         public ScalarEnlistment(string enlistmentRoot, string workingDirectory, string repoUrl, string gitBinPath, GitAuthentication authentication)
             : base(
@@ -171,6 +173,26 @@ namespace Scalar.Common
             }
 
             return true;
+        }
+
+        public static bool IsUnattended(ITracer tracer)
+        {
+            try
+            {
+                return Environment.GetEnvironmentVariable(ScalarConstants.UnattendedEnvironmentVariable) == "1";
+            }
+            catch (SecurityException e)
+            {
+                if (tracer != null)
+                {
+                    EventMetadata metadata = new EventMetadata();
+                    metadata.Add("Area", nameof(ScalarEnlistment));
+                    metadata.Add("Exception", e.ToString());
+                    tracer.RelatedError(metadata, "Unable to read environment variable " + ScalarConstants.UnattendedEnvironmentVariable);
+                }
+
+                return false;
+            }
         }
     }
 }
