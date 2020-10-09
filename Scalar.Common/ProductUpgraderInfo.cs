cc744e1a793f94e1c8c14bc361c208e105d5e5c0
Chris Darroch <chrisd8088@github.com>
2020-10-09 00:45:51 -0700

    ProductUpgraderInfo: undo partial classes
    The GVFS/GVFS.Common/ProductUpgraderInfo.cs file was split into
    two and GVFS/GVFS.Common/ProductUpgraderInfo.Shared.cs was created
    in commit d5d9e7d3c5f822436d4ba7283e18581fc4d528f7, in order to
    share the IsLocalUpgradeAvailable() method and some constant path
    strings with the GVFS.Hooks project.
    
    Given that there is no longer a corresponding Scalar version of
    the GVFS.Hooks project, there is no need to split out certain
    (static) methods from the others in any classes.  (The GVFS.Hooks
    project was removed in commit
    f12effc371d7b1457ab58c6d44959632404e5998 in PR #23.)
    
    Further, the IsLocalUpgradeAvailable() method's only caller was
    in the GVFS.Hooks project, so we can just drop that method and
    one private one as well.  We do merge another private method
    and the path constants into ProductUpgraderInfo.cs and then
    remove the ProductUpgraderInfo.Shared.cs file.

--- Scalar.Common/ProductUpgraderInfo.cs
+++ Scalar.Common/ProductUpgraderInfo.cs
@@ -5,8 +5,20 @@ using System.IO;
 
 namespace Scalar.Common
 {
-    public partial class ProductUpgraderInfo
+    public class ProductUpgraderInfo
     {
+        public const string UpgradeDirectoryName = "Scalar.Upgrade";
+        public const string LogDirectory = "UpgraderLogs";
+        public const string DownloadDirectory = "Downloads";
+
+        /// <summary>
+        /// This is the name of the directory that the Upgrader Application is copied to
+        /// when running upgrade. It is called "Tools", as this is what the directory was
+        /// originally named, but has since been renamed in code to be more descriptive.
+        /// </summary>
+        public const string ApplicationDirectory = "Tools";
+        public const string HighestAvailableVersionFileName = "HighestAvailableVersion";
+
         private ITracer tracer;
         private PhysicalFileSystem fileSystem;
 
@@ -97,6 +109,11 @@ namespace Scalar.Common
                     this.tracer.RelatedInfo($"{nameof(this.RecordHighestAvailableVersion)}: Created upgrade reminder marker file");
                 }
             }
+        }
+
+        private static string GetHighestAvailableVersionFilePath(string highestAvailableVersionDirectory)
+        {
+            return Path.Combine(highestAvailableVersionDirectory, HighestAvailableVersionFileName);
         }
     }
 }
