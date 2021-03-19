30439736a2be2c3995e7d2a103fcc30089a1787c
Derrick Stolee <dstolee@microsoft.com>
2021-03-19 10:39:59 -0400

    macOS: Fix local build script
    This gets around the multiple sources problem, but has the issue
    that every build will download the installers fresh.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Scalar.Installer.Mac/dist.sh
+++ Scalar.Installer.Mac/dist.sh
@@ -6,8 +6,8 @@ if [ -z "${SCALAR_PKG}" ]; then
   exit 1
 fi
 
-GIT_PKG=$2
-if [ -z "${GIT_PKG}" ]; then
+GIT_PKG_VERSION=$2
+if [ -z "${GIT_PKG_VERSION}" ]; then
   echo "Error: Git installer package not specified"
   exit 1
 fi
@@ -47,13 +47,13 @@ function CopyGit()
 {
     GIT_DESTINATION="${OUT_DIR}/Git"
 
-    if [ ! -f "${GIT_PKG}" ] ; then
-        echo "Error: Could not find Git for Mac package at ${GIT_PKG}."
-        exit 1
-    fi
-
     mkdir -p "${GIT_DESTINATION}" || exit 1
-    cp -Rf "${GIT_PKG}" "${GIT_DESTINATION}" || exit 1
+
+    nuget install GitForMac.GVFS.Installer -DependencyVersion "$GIT_PKG_VERISON" -Source "https://pkgs.dev.azure.com/gvfs/ci/_packaging/Dependencies/nuget/v3/index.json" -OutputDirectory "$GIT_DESTINATION"
+    unzip "$GIT_DESTINATION/GitForMac.GVFS.Installer.$GIT_PKG_VERSION/GitForMac.GVFS.Installer.$GIT_PKG_VERSION.nupkg"
+    mv "$GIT_DESTINATION/GitForMac.GVFS.Installer.$GIT_PKG_VERSION/GitForMac.GVFS.Installer.$GIT_PKG_VERSION/tools" "$GIT_DESTINATION/tools"
+    GIT_PKG="$GIT_DESTINATION/tools/*pkg"
+    rm -rf "$GIT_DESTINATION/GitForMac.GVFS.Installer.$GIT_PKG_VERSION/"
 }
 
 function CopyGcmCore()
