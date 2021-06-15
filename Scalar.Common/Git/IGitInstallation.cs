75d70c1d2b670ca3f9ec6b222462c0c64c3f98a0
Derrick Stolee <dstolee@microsoft.com>
2021-06-15 16:44:36 -0400

    Delete Scalar.Common
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Scalar.Common/Git/IGitInstallation.cs
+++
@@ -1,7 +0,0 @@
-namespace Scalar.Common.Git
-{
-    public interface IGitInstallation
-    {
-        string GetInstalledGitBinPath();
-    }
-}
--- Scalar.Common/Tracing/IQueuedPipeStringWriterEventSink.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Scalar.Common.Tracing
-{
-    public interface IQueuedPipeStringWriterEventSink
-    {
-        void OnStateChanged(QueuedPipeStringWriter writer, QueuedPipeStringWriterState state, Exception exception);
-    }
-}
